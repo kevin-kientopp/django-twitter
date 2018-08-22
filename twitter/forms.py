408f932e5e95dfab329290e8f64141d2534ec978
Kevin Kientopp <kevinkientopp@gmail.com>
2018-08-21 21:44:24 -0600

    remove unused signup form

--- twitter/forms.py
+++ twitter/forms.py
@@ -2,21 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Tweet
 
-class SignupForm(forms.Form):
-    email = forms.EmailField()
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirm', widget=forms.PasswordInput)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password1 = cleaned_data.get('password1')
-        password2 = cleaned_data.get('password2')
-        if password1 != password2:
-            raise forms.ValidationError('Passwords do not match.')
-        email = cleaned_data.get('email')
-        if User.objects.get(email=email):
-            raise forms.ValidationError('That email is already in use.')
-
 class TweetForm(forms.ModelForm):
     body = forms.CharField(
             max_length = 140,
