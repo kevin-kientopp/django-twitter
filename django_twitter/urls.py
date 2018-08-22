5f86fd134bf9d953d525cb62cb14103d185d0d2a
Kevin Kientopp <kevinkientopp@gmail.com>
2018-08-21 21:31:22 -0600

    root path now redirects to twitter app

--- django_twitter/urls.py
+++ django_twitter/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    path('', lambda r: HttpResponseRedirect('twitter/')),
     path('twitter/', include('twitter.urls')),
     path('admin/', admin.site.urls),
 ]
