b31fbc3021e742614782f39cb1d6ee1a9c4c642d
Kevin Kientopp <kevinkientopp@gmail.com>
2018-09-16 10:08:56 -0600

    Add test for index view

--- twitter/tests.py
+++ twitter/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+class ViewsTestCase(TestCase):
+    def test_index(self):
+        """When an unauthenticed user visits the index, the index page is rendered."""
+        c = Client()
+        response = c.get(reverse('twitter:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Welcome to my Twitter clone!')
+        self.assertContains(response, 'Sign up')
+        self.assertContains(response, 'Log in')
+
