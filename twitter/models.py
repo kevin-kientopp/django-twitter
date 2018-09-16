04d709089a29702c0ddbb795f9866b0a1e703a08
Kevin Kientopp <kevinkientopp@gmail.com>
2018-09-16 13:57:49 -0600

    Cleanup tests. Fix datetimefield warning

--- twitter/models.py
+++ twitter/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Tweet(models.Model):
     body = models.CharField(max_length=140)
     auth_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    published_date = models.DateTimeField(default = timezone.now())
+    published_date = models.DateTimeField(default = timezone.now)
 
     def __str__(self):
         return self.body
--- twitter/tests.py
+++ twitter/tests.py
@@ -3,53 +3,55 @@ from django.urls import reverse
 
 # Create your tests here.
 class ViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_index(self):
         """When an unauthenticed user visits the index, the index page is rendered."""
-        c = Client()
-        response = c.get(reverse('twitter:index'))
+        response = self.client.get(reverse('twitter:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Welcome to my Twitter clone!')
         self.assertContains(response, 'Sign up')
         self.assertContains(response, 'Log in')
 
-    def test_signup_and_login(self):
-        c = Client()
-        credentials = {
-                'username':'test123',
-                'password1':'P@ssw0rd1',
-                'password2':'P@ssw0rd1',
-                };
-        response = c.post(reverse('twitter:signup'), credentials)
+    def test_signup(self):
+        creds = create_signup_creds();
+        response = self.client.post(reverse('twitter:signup'), creds)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('twitter:home'))
 
     def test_logout(self):
-        c = Client()
-        credentials = {
-                'username':'test123',
-                'password1':'P@ssw0rd1',
-                'password2':'P@ssw0rd1',
-                };
-        response = c.post(reverse('twitter:signup'), credentials)
-        response = c.post(reverse('twitter:logout'))
+        creds = create_signup_creds();
+        response = self.client.post(reverse('twitter:signup'), creds)
+        response = self.client.post(reverse('twitter:logout'))
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('twitter:index'))
 
     def test_login(self):
-        c = Client()
-        signup_credentials = {
-                'username':'test123',
-                'password1':'P@ssw0rd1',
-                'password2':'P@ssw0rd1',
-                };
-        response = c.post(reverse('twitter:signup'), signup_credentials)
-        response = c.post(reverse('twitter:logout'))
-        login_credentials = {
+        signup_creds = create_signup_creds();
+        response = self.client.post(reverse('twitter:signup'), signup_creds)
+        response = self.client.post(reverse('twitter:logout'))
+        login_creds = {
                 'username':'test123',
                 'password':'P@ssw0rd1',
                 };
-        response = c.post(reverse('twitter:login'), login_credentials)
+        response = self.client.post(reverse('twitter:login'), login_creds)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('twitter:home'))
 
+    def test_tweet(self):
+        creds = create_signup_creds();
+        response = self.client.post(reverse('twitter:signup'), creds)
+        tweet = {
+                'body': 'This is a test...'
+                };
+        response = self.client.post(reverse('twitter:home'), tweet)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('twitter:home'))
 
+def create_signup_creds():
+    return {
+            'username':'test123',
+            'password1':'P@ssw0rd1',
+            'password2':'P@ssw0rd1',
+            };
