6bb7b86bada722e39e0c7e4ff737f22a8931c624
Kevin Kientopp <kevinkientopp@gmail.com>
2018-09-16 13:33:07 -0600

    Add more tests

--- twitter/tests.py
+++ twitter/tests.py
@@ -12,3 +12,44 @@ class ViewsTestCase(TestCase):
         self.assertContains(response, 'Sign up')
         self.assertContains(response, 'Log in')
 
+    def test_signup_and_login(self):
+        c = Client()
+        credentials = {
+                'username':'test123',
+                'password1':'P@ssw0rd1',
+                'password2':'P@ssw0rd1',
+                };
+        response = c.post(reverse('twitter:signup'), credentials)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('twitter:home'))
+
+    def test_logout(self):
+        c = Client()
+        credentials = {
+                'username':'test123',
+                'password1':'P@ssw0rd1',
+                'password2':'P@ssw0rd1',
+                };
+        response = c.post(reverse('twitter:signup'), credentials)
+        response = c.post(reverse('twitter:logout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('twitter:index'))
+
+    def test_login(self):
+        c = Client()
+        signup_credentials = {
+                'username':'test123',
+                'password1':'P@ssw0rd1',
+                'password2':'P@ssw0rd1',
+                };
+        response = c.post(reverse('twitter:signup'), signup_credentials)
+        response = c.post(reverse('twitter:logout'))
+        login_credentials = {
+                'username':'test123',
+                'password':'P@ssw0rd1',
+                };
+        response = c.post(reverse('twitter:login'), login_credentials)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('twitter:home'))
+
+
